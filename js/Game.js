c5180f665ce3f28fd44de353fd92baa20da72a4a
Anshika Jaiswal <79703623+Anshika0101@users.noreply.github.com>
2021-06-04 17:13:43 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -56,7 +56,7 @@ class Game {
               var index =0;
         
               //x and y position of the cars
-              var x =200;
+              var x;
               var y;
         
               for(var plr in allPlayers){
